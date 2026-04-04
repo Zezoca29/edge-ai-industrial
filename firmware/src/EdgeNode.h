edac934a7c8d83fd2fcc7c7cbd3243fe20dee9b0
Kaique Augusto <kaiquezeza@gmail.com>
2026-04-04 19:48:06 -0300

    feat(firmware): add EdgeNode orchestrator with begin/loop cycle

---
+++ firmware/src/EdgeNode.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "sensors/sensor_manager.h"
+#include "inference/inference_engine.h"
+#include "communication/wifi_manager.h"
+#include "communication/mqtt_client.h"
+
+class EdgeNode {
+public:
+    void begin();
+    void loop();
+
+private:
+    WifiManager     _wifi;
+    MqttClient      _mqtt;
+    SensorManager   _sensors;
+    InferenceEngine _inference;
+
+    unsigned long _lastPublishMs = 0;
+    unsigned long _lastStatusMs  = 0;
+    unsigned long _startMs       = 0;
+};
